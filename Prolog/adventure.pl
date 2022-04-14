3c7f94f26fab15eb50f1e209874343c9d948e3df
Jakub Robaczewski <kubarob13@gmail.com>
2022-04-14 13:16:45 +0200

    Added tomb map

--- Prolog/adventure.pl
+++ Prolog/adventure.pl
@@ -1,20 +1,50 @@
-/* <The name of this game>, by <your name goes here>. */
+/* Pieseły i Locheły, by Jakub Robaczewski, Paweł Muller, Marianna Gromadzka. */
 
 :- dynamic i_am_at/1, at/2, holding/1.
 :- retractall(at(_, _)), retractall(holding(_)), retractall(i_am_at(_)), retractall(alive(_)).
 
-i_am_at(kitchen).
+i_am_at(entance).
 
-path(kitchen, n, kitchen).
+% Map of the Egyptian tomb
+path(entance, n, antechamber).
+path(antechamber, n, altar_room).
+path(antechamber, s, entance).
+path(antechamber, w, jar_room).
+path(antechamber, e, attendant_room).
+path(jar_room, e, antechamber).
+path(jar_room, w, accolite_chamber_1).
+path(attendant_room, w, antechamber).
+path(attendant_room, n, corridor).
+path(corridor, n, false_floor_room).
+path(corridor, w, altar_room).
+path(accolite_chamber_1, n, accolite_chamber_2).
+path(accolite_chamber_2, s, accolite_chamber_1).
+path(accolite_chamber_2, e, serket_chamber).
+path(altar_room, s, antechamber).
+path(altar_room, e, corridor).
+path(false_floor_room, s, corridor).
+path(false_floor_room, n, trap_corridor).
+path(trap_corridor, s, false_floor_room).
+path(trap_corridor, n, tresure_room).
+path(serket_chamber, w, accolite_chamber_2).
+path(serket_chamber, n, guardian).
+path(guardian, s, serket_chamber).
+path(guardian, n, sarcophagus).
+path(tresure_room, s, trap_corridor).
+path(tresure_room, n, hidden_exit).
+path(sarcophagus, s, guardian).
+path(sarcophagus, e, hidden_exit).
+path(hidden_exit, n, sarcophagus).
+path(hidden_exit, s, tresure_room).
 
-at(bowl, kitchen).
-at(cornflakes, kitchen).
-at(milk, kitchen).
+% at(bowl, kitchen).
+% at(cornflakes, kitchen).
+% at(milk, kitchen).
 
-crafting_recipe(bowl, milk, bowl_with_milk).
-crafting_recipe(bowl_with_milk, cornflakes, monster_meal).
-crafting_recipe(bowl, cornflakes, bowl_with_cornflakes).
-crafting_recipe(bowl_with_cornflakes, milk, breakfast).
+% crafting_recipe(bowl, milk, bowl_with_milk).
+% crafting_recipe(bowl_with_milk, cornflakes, monster_meal).
+% crafting_recipe(bowl, cornflakes, bowl_with_cornflakes).
+% crafting_recipe(bowl_with_cornflakes, milk, breakfast).
 
 /* These rules describe how to pick up an object. */
 
@@ -50,20 +80,20 @@ drop(_) :-
         write('You aren''t holding it!'),
         nl.
 
-craft(X, Y) :-
-        holding(X),
-        holding(Y),
-        crafting_recipe(X, Y, A),
-        !,
-        assert(holding(A)),
-        retract(holding(X)),
-        retract(holding(Y)),
-        write('You created '), write(A),
-        look.
+% craft(X, Y) :-
+%         holding(X),
+%         holding(Y),
+%         crafting_recipe(X, Y, A),
+%         !,
+%         assert(holding(A)),
+%         retract(holding(X)),
+%         retract(holding(Y)),
+%         write('You created '), write(A),
+%         look.
 
-craft(_, _) :-
-        write('Something went wrong!'),
-        nl.
+% craft(_, _) :-
+%         write('Something went wrong!'),
+%         nl.
 
 /* These rules define the direction letters as calls to go/1. */
 
@@ -100,8 +130,7 @@ look :-
         notice_holding_objects.
 
 
-/* These rules set up a loop to mention all the objects
-   in your vicinity. */
+/* These rules set up a loop to mention all the objects in your vicinity. */
 
 notice_objects_at(Place) :-
         at(X, Place),
@@ -156,8 +185,7 @@ start :-
         look.
 
 
-/* These rules describe the various rooms.  Depending on
-   circumstances, a room may have more than one description. */
+/* These rules describe the various rooms.  Depending on circumstances, a room may have more than one description. */
 
-describe(kitchen) :- write('You are in kitchen.'), nl.
+describe(entance) :- write('Stoisz w tunelu prowadzącym do grobowca, przed tobą znajdują się uchylone wrota.'), nl.
 
