a63cb0f377aeff230b388d11c22ca493cdabfab6
Jakub Robaczewski <kubarob13@gmail.com>
2022-05-18 21:59:21 +0200

    Quickfix

--- Haskell/src/ItemActions.hs
+++ Haskell/src/ItemActions.hs
@@ -1,25 +1,31 @@
 module ItemActions where
     import qualified Data.List as List
-    import State ( State(comment, holding, items, you) )
-    import Character (Character(location))
+    import State ( State(comment, enemies, holding, items, you) )
+    import Character (Character(location, name))
     import Combat (die)
     import Items (Item(name, itemLocation), floatingCrystal)
     
     take :: String -> State -> State
-    take itemName state = case List.find (\x -> itemName == name x && location (you state) == itemLocation x) (items state) of 
-        Nothing -> state { comment = ["There is no " ++ itemName ++ " in this room"] }
-        Just item | item == floatingCrystal -> die (you state) state { comment = ["You tried to grab the crystal, but floor collapsed under you. You fall into spikes."] }
-                  | otherwise -> state { holding = item {itemLocation=""}:holding state, items = List.delete item (items state), comment = ["You took " ++ itemName ++ " from the ground"]  }
+    take itemName state = case List.find (\x -> location (you state) == location x) (enemies state) of
+        Nothing -> case List.find (\x -> itemName == Items.name x && location (you state) == itemLocation x) (items state) of 
+            Nothing -> state { comment = ["There is no " ++ itemName ++ " in this room"] }
+            Just item | item == floatingCrystal -> die (you state) state { comment = ["You tried to grab the crystal, but floor collapsed under you. You fall into spikes."] }
+                      | otherwise -> state { holding = item {itemLocation=""}:holding state, items = List.delete item (items state), comment = ["You took " ++ itemName ++ " from the ground"]  }
+        Just enemy -> state { comment = ["You cannot take items in room when there is " ++ Character.name enemy ++ " in it."]}
 
     drop :: String -> State -> State
-    drop itemName state = case List.find (\x -> itemName == name x) (holding state) of
-        Nothing -> state { comment = ["You don't have " ++ itemName ++ " in your inventory"] }
-        Just item -> state { holding = List.delete item (holding state), items = item {itemLocation=location (you state)}:items state, comment = ["You drop " ++ itemName ++ " to ground"]  }
+    drop itemName state = case List.find (\x -> location (you state) == location x) (enemies state) of
+        Nothing -> case List.find (\x -> itemName == Items.name x) (holding state) of
+            Nothing -> state { comment = ["You don't have " ++ itemName ++ " in your inventory"] }
+            Just item -> state { holding = List.delete item (holding state), items = item {itemLocation=location (you state)}:items state, comment = ["You drop " ++ itemName ++ " to ground"]  }
+        Just enemy -> state { comment = ["You cannot drop items in room when there is " ++ Character.name enemy ++ " in it."]}
 
     inventory :: State -> State
-    inventory state = state {comment = "Your inventory:" : map name (holding state)}
+    inventory state = state {comment = "Your inventory:" : map Items.name (holding state)}
 
     search :: State -> State
-    search state = case map name (filter (\x -> itemLocation x == location (you state)) (items state)) of
-        [] -> state { comment = ["There is nothing here"]}
-        items -> state { comment = "You found these items:" : items }
+    search state = case List.find (\x -> location (you state) == location x) (enemies state) of
+        Nothing -> case map Items.name (filter (\x -> itemLocation x == location (you state)) (items state)) of
+            [] -> state { comment = ["There is nothing here"]}
+            items -> state { comment = "You found these items:" : items }
+        Just enemy -> state { comment = ["You cannot search this room when there is " ++ Character.name enemy ++ " in it."]}
