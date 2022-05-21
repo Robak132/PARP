c429a9fd74a6285f0efc2dbcec34d39b672ba4e2
Jakub Robaczewski <kubarob13@gmail.com>
2022-05-21 12:26:32 +0200

    Finished

--- Haskell/src/Traps.hs
+++ Haskell/src/Traps.hs
@@ -2,13 +2,12 @@ module Traps where
     data Trap = Trap {
         location_from :: String,
         location_to :: String,
-        strengthModifier :: Int,
         damage :: Int,
         dodge :: Int
     } deriving (Show)
 
     bladeTrap :: Trap
-    bladeTrap = Trap "false_floor_room" "trap_corridor_a" 0 6 10
+    bladeTrap = Trap "false_floor_room" "trap_corridor_a" 6 10
 
     slabTrap :: Trap
-    slabTrap = Trap "trap_corridor_b" "treasure_room" 0 2 6
+    slabTrap = Trap "trap_corridor_b" "treasure_room" 2 6
